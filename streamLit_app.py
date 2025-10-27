51b9b4b374016cb103346cea4a80a84fc9bf6d07
nayabayesha179 <165317610+nayabayesha179@users.noreply.github.com>
2025-10-27 20:19:04 +0500

    Update streamLit_app.py

--- streamLit_app.py
+++ streamLit_app.py
@@ -1,1 +1,9 @@
 
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch
+
+# Load the trained model
+model_path = "bert_emotion_model"  # folder path (unzipped)
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+model = AutoModelForSequenceClassification.from_pretrained(model_path)
+model.eval()
